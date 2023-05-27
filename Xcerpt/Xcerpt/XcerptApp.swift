9acc68fcd299e8d083104f33324f6f2328968829
Brian Ho <brnho@stanford.edu>
2023-05-27 09:26:55 -0700

    Initial commit

---
+++ Xcerpt/Xcerpt/XcerptApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcerptApp.swift
+//  Xcerpt
+//
+//  Created by Brian Ho on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct XcerptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
